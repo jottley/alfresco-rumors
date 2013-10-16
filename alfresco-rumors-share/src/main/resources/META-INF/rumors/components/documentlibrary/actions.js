c2fe9f943eb7e52f82760cd58c388eab2d51befc
Jared Ottley <jottley@gmail.com>
2013-10-16 15:32:39 -0600

    Add page refresh after action is taken

--- alfresco-rumors-share/src/main/resources/META-INF/rumors/components/documentlibrary/actions.js
+++ alfresco-rumors-share/src/main/resources/META-INF/rumors/components/documentlibrary/actions.js
@@ -22,177 +22,230 @@
  * 
  * @author jottley
  */
-(function() {
-   
-   /*
-    * YUI aliases
-    */
-   var Dom = YAHOO.util.Dom,
-       Event = YAHOO.util.Event,
-       KeyListener = YAHOO.util.KeyListener;
+(function()
+{
 
-   /**
-    * Enable a node as a XMPP Client Node
-    *
-    * @method enableXMPPNodeAction
-    * @param record {object} Object literal representing the file or folder on which the work should be performed
-    */
-   YAHOO.Bubbling.fire("registerAction", {
-      actionName : "enableXMPPNodeAction",
-      fn : function dlA_enableXMPPNodeAction(record) {
-         
-         var me = this;
-         
-         Alfresco.Rumors.showMessage({
-             text: "Enabling Rumors", 
-             displayTime: 0,
-             showSpinner: true
-          });
-         
-         var success = {
-        		 fn : function(response)
-        		 {
-        	         var _success = {
-        	        		 fn : function(response)
-        	        		 {
-        	        			   var __success = {
-        	          	        		 fn : function(response)
-        	          	        		 {
-        	          	        			 Alfresco.util.PopupManager
-        	          							.displayMessage( {
-        	          								text : "Enabled"
-        	          							});
-        	          	        		 },
-        	          	        		 scope: this
-        	          	         }
-        	          	         
-        	          	         var __failure = {
-        	          	        		 fn : function(response)
-        	          	        		 {
-        	          	        			 Alfresco.util.PopupManager
-        	          							.displayMessage( {
-        	          								text : "failure"
-        	          							});
-        	          	        		 },
-        	          	        		 scope: this
-        	          	         }
-        	          			 
-        	          			 Alfresco.util.Ajax.jsonRequest({
-        	          	        	 url: Alfresco.constants.PROXY_URI + 'xmpp/node/message',
-        	          	        	 method: 'POST',
-        	          	        	 dataObj: {
-        	          	        		 nodeRef: record.nodeRef,
-        	          	        		 message: 'You have XMPP enabled ' + record.jsNode.properties.title
-        	          	        	 },
-        	          	        	 requestContentType: Alfresco.util.Ajax.JSON,
-        	          	        	 successCallback: __success,
-        	          	        	 failureCallback: __failure
-        	          	         });
-        	        		 },
-        	        		 scope: this
-        	         }
-        	         
-        	         var _failure = {
-        	        		 fn : function(response)
-        	        		 {
-        	        			 Alfresco.util.PopupManager
-        							.displayMessage( {
-        								text : "failure"
-        							});
-        	        		 },
-        	        		 scope: this
-        	         }
-        			 
-        			 Alfresco.util.Ajax.jsonRequest({
-        	        	 url: Alfresco.constants.PROXY_URI + 'xmpp/node/roster/add',
-        	        	 method: 'POST',
-        	        	 dataObj: {
-        	        		 nodeRef: record.nodeRef,
-        	        		 recipricate: true
-        	        	 },
-        	        	 requestContentType: Alfresco.util.Ajax.JSON,
-        	        	 successCallback: _success,
-        	        	 failureCallback: _failure
-        	         });
-        		 },
-        		 scope: this
-         }
-         
-         var failure = {
-        		 fn : function(response)
-        		 {
-        			 Alfresco.util.PopupManager
-						.displayMessage( {
-							text : "failure"
-						});
-        		 },
-        		 scope: this
-         }
-         
-         Alfresco.util.Ajax.jsonRequest({
-        	 url: Alfresco.constants.PROXY_URI + 'xmpp/node/enable',
-        	 method: 'POST',
-        	 dataObj: {
-        		 nodeRef: record.nodeRef
-        	 },
-        	 requestContentType: Alfresco.util.Ajax.JSON,
-        	 successCallback: success,
-        	 failureCallback: failure
-         });
-      }
-   })
-   
-   
-   /**
-    * Disable a node as a XMPP Client Node
-    *
-    * @method diableXMPPNodeAction
-    * @param record {object} Object literal representing the file or folder on which the work should be performed
-    */
-   YAHOO.Bubbling.fire("registerAction", {
-      actionName : "disableXMPPNodeAction",
-      fn : function dlA_disableXMPPNodeAction(record) {
-         
-         var me = this;
-         
-         Alfresco.Rumors.showMessage({
-             text: "Disabling Rumors", 
-             displayTime: 0,
-             showSpinner: true
-          });
-         
-         var success = {
-        		 fn : function(response)
-        		 {
-        			 Alfresco.util.PopupManager
-						.displayMessage( {
-							text : "Disabled"
-						});
-        		 },
-        		 scope: this
-         }
-         
-         var failure = {
-        		 fn : function(response)
-        		 {
-        			 Alfresco.util.PopupManager
-						.displayMessage( {
-							text : "failure"
-						});
-        		 },
-        		 scope: this
-         }
-         
-         Alfresco.util.Ajax.jsonRequest({
-        	 url: Alfresco.constants.PROXY_URI + 'xmpp/node/disable',
-        	 method: 'POST',
-        	 dataObj: {
-        		 nodeRef: record.nodeRef
-        	 },
-        	 requestContentType: Alfresco.util.Ajax.JSON,
-        	 successCallback: success,
-        	 failureCallback: failure
-         });
-      }
-   })
+    /*
+     * YUI aliases
+     */
+    var Dom = YAHOO.util.Dom, Event = YAHOO.util.Event, KeyListener = YAHOO.util.KeyListener;
+
+    /**
+     * Enable a node as a XMPP Client Node
+     * 
+     * @method enableXMPPNodeAction
+     * @param record
+     *            {object} Object literal representing the file or folder on
+     *            which the work should be performed
+     */
+    YAHOO.Bubbling
+                    .fire("registerAction", {
+                        actionName : "enableXMPPNodeAction",
+                        fn : function dlA_enableXMPPNodeAction(record)
+                        {
+
+                            var me = this;
+
+                            Alfresco.Rumors.showMessage({
+                                text : "Enabling Rumors",
+                                displayTime : 0,
+                                showSpinner : true
+                            });
+
+                            var success = {
+                                fn : function(response)
+                                {
+                                    var _success = {
+                                        fn : function(response)
+                                        {
+                                            var __success = {
+                                                fn : function(response)
+                                                {
+                                                    Alfresco.util.PopupManager
+                                                                    .displayMessage({
+                                                                        text : "Enabled"
+                                                                    });
+                                                    _pageRefresh(record.nodeRef);
+                                                },
+                                                scope : this
+                                            }
+
+                                            var __failure = {
+                                                fn : function(response)
+                                                {
+                                                    Alfresco.util.PopupManager
+                                                                    .displayMessage({
+                                                                        text : "failure"
+                                                                    });
+                                                },
+                                                scope : this
+                                            }
+
+                                            Alfresco.util.Ajax
+                                                            .jsonRequest({
+                                                                url : Alfresco.constants.PROXY_URI
+                                                                                + 'xmpp/node/message',
+                                                                method : 'POST',
+                                                                dataObj : {
+                                                                    nodeRef : record.nodeRef,
+                                                                    message : 'You have XMPP enabled '
+                                                                                    + record.jsNode.properties.title
+                                                                },
+                                                                requestContentType : Alfresco.util.Ajax.JSON,
+                                                                successCallback : __success,
+                                                                failureCallback : __failure
+                                                            });
+                                        },
+                                        scope : this
+                                    }
+
+                                    var _failure = {
+                                        fn : function(response)
+                                        {
+                                            Alfresco.util.PopupManager
+                                                            .displayMessage({
+                                                                text : "failure"
+                                                            });
+                                        },
+                                        scope : this
+                                    }
+
+                                    Alfresco.util.Ajax
+                                                    .jsonRequest({
+                                                        url : Alfresco.constants.PROXY_URI
+                                                                        + 'xmpp/node/roster/add',
+                                                        method : 'POST',
+                                                        dataObj : {
+                                                            nodeRef : record.nodeRef,
+                                                            recipricate : true
+                                                        },
+                                                        requestContentType : Alfresco.util.Ajax.JSON,
+                                                        successCallback : _success,
+                                                        failureCallback : _failure
+                                                    });
+                                },
+                                scope : this
+                            }
+
+                            var failure = {
+                                fn : function(response)
+                                {
+                                    Alfresco.util.PopupManager.displayMessage({
+                                        text : "failure"
+                                    });
+                                },
+                                scope : this
+                            }
+
+                            Alfresco.util.Ajax.jsonRequest({
+                                url : Alfresco.constants.PROXY_URI
+                                                + 'xmpp/node/enable',
+                                method : 'POST',
+                                dataObj : {
+                                    nodeRef : record.nodeRef
+                                },
+                                requestContentType : Alfresco.util.Ajax.JSON,
+                                successCallback : success,
+                                failureCallback : failure
+                            });
+                        }
+                    })
+
+    /**
+     * Disable a node as a XMPP Client Node
+     * 
+     * @method diableXMPPNodeAction
+     * @param record
+     *            {object} Object literal representing the file or folder on
+     *            which the work should be performed
+     */
+    YAHOO.Bubbling.fire("registerAction", {
+        actionName : "disableXMPPNodeAction",
+        fn : function dlA_disableXMPPNodeAction(record)
+        {
+
+            var me = this;
+
+            Alfresco.Rumors.showMessage({
+                text : "Disabling Rumors",
+                displayTime : 0,
+                showSpinner : true
+            });
+
+            var success = {
+                fn : function(response)
+                {
+                    Alfresco.util.PopupManager.displayMessage({
+                        text : "Disabled"
+                    });
+                    _pageRefresh(record.nodeRef);
+                },
+                scope : this
+            }
+
+            var failure = {
+                fn : function(response)
+                {
+                    Alfresco.util.PopupManager.displayMessage({
+                        text : "failure"
+                    });
+                },
+                scope : this
+            }
+
+            Alfresco.util.Ajax.jsonRequest({
+                url : Alfresco.constants.PROXY_URI + 'xmpp/node/disable',
+                method : 'POST',
+                dataObj : {
+                    nodeRef : record.nodeRef
+                },
+                requestContentType : Alfresco.util.Ajax.JSON,
+                successCallback : success,
+                failureCallback : failure
+            });
+        }
+    })
+
+    /**
+     * Displays the corresponding details page for the current node
+     * 
+     * @method _navigateForward
+     * @private
+     */
+    function _pageRefresh(nodeRef)
+    {
+        /* Was the return page specified */
+        var returnPath = Alfresco.util
+                        .getQueryStringParameter("return", location.hash
+                                        .replace("#", ""));
+        if (returnPath) {
+            // remove the 'file' querystring param, which causes a file to be
+            // highlighted
+            returnPath = returnPath.replace(/\?file=[^&#]*/, "");
+
+            window.location.href = location.protocol + "//" + location.host
+                            + Alfresco.constants.URL_PAGECONTEXT + returnPath;
+        }
+        /*
+         * Did we come from the document library? If so, then direct the user
+         * back there
+         */
+        else if (document.location.toString().match(/documentlibrary([#?]|$)/)
+                        || document.location.toString()
+                                        .match(/repository([#?]|$)/)) {
+            /*
+             * Send the user back to the last page - this could be either the
+             * document list or document details page
+             * 
+             * We could use window.history.back(), but that does not trigger the
+             * document actions and metadata to be reloaded
+             */
+            window.location.reload();
+        }
+        else {
+            // go forward to the appropriate details page for the node
+            window.location.href = Alfresco.util
+                            .siteURL("document-details?nodeRef=" + nodeRef);
+        }
+    }
 })();
